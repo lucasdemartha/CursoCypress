b5629d271cd34f34b6975e132a285896628a5cfb
lucasdemartha <lucasribm@gmail.com>
2024-03-04 15:33:36 -0300

    sync

--- Cypress/cypress/e2e/sync.cy.js
+++ Cypress/cypress/e2e/sync.cy.js
@@ -19,12 +19,22 @@ describe('Esperas...', () => {
         cy.get('#novoCampo').type('funciona')
     })
 
-    it('Deve fazer retrys', () => {
+    it.skip('Deve fazer retrys', () => {
 
         cy.get('#buttonDelay').click()
         cy.get('#novoCampo')
             .should('exist')
             .type('funciona')
+    })
+
+    it('Uso do find', () => {
+        cy.get('#buttonList').click()
+        cy.get('#lista li')
+            .find('span')
+            .should('contain', 'Item 1')
+
+        cy.get('#lista li span')
+            .should('contain', 'Item 2')
     })
 
 })
